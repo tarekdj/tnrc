ef0e8f4c80cdb758f73d2ae85babf340fce5bb82
tarekdj <tarekdj@gmail.com>
2017-11-02 12:38:13 +0100

    Move to tnrc class.

--- tnrc.js
+++ tnrc.js
@@ -4,46 +4,48 @@ const cheerioTableparser = require('cheerio-tableparser');
 const config = require('./config');
 
 module.exports = {
-  getDataByRC: async function getDataByRC(num) {
-    const browser = await puppeteer.launch();
+  tnrc: class tnrc {
+    static async getDataByRC(num) {
+      const browser = await puppeteer.launch();
 
-    const page = await browser.newPage();
-    // Perform a new search.
-    await page.goto(config.URL);
-    await page.click(config.SEARCH_INPUT);
-    await page.type(num);
-    await page.click(config.SEACH_RESULT_SELECTOR);
-    // Wait for results and show details.
-    await page.waitForNavigation();
-    await page.evaluate((sel) => {
-      choixDocuments(0);
-      return true;
-    }, config.VIEW_LINK);
-    await page.waitForNavigation();
+      const page = await browser.newPage();
+      // Perform a new search.
+      await page.goto(config.URL);
+      await page.click(config.SEARCH_INPUT);
+      await page.type(num);
+      await page.click(config.SEACH_RESULT_SELECTOR);
+      // Wait for results and show details.
+      await page.waitForNavigation();
+      await page.evaluate((sel) => {
+        choixDocuments(0);
+        return true;
+      }, config.VIEW_LINK);
+      await page.waitForNavigation();
 
-    let data = await page.content();
-    await browser.close();
-    if (data.match(/Aucun résultat répondant à votre recherche/)) {
-      return false;
-    }
-    let $ = cheerio.load(data);
-    cheerioTableparser($);
-    let _data = $(config.TBL_SELECTOR).parsetable(true, true, true);  //@todo: handle no results.
-
-    let result = {
-      "reg_number": _data[2][0],
-      "legal_form": _data[2][1],
-      "reg_status": _data[2][2],
-      "company_name": {
-        "FR": _data[2][3],
-        "AR": _data[4][3]
-      },
-      "commercial_name": _data[2][4],
-      "address": {
-        "FR": _data[2][5],
-        "AR": _data[4][5]
+      let data = await page.content();
+      await browser.close();
+      if (data.match(/Aucun résultat répondant à votre recherche/)) {
+        return false;
       }
-    };
-    return result;
+      let $ = cheerio.load(data);
+      cheerioTableparser($);
+      let _data = $(config.TBL_SELECTOR).parsetable(true, true, true);  //@todo: handle no results.
+
+      let result = {
+        "reg_number": _data[2][0],
+        "legal_form": _data[2][1],
+        "reg_status": _data[2][2],
+        "company_name": {
+          "FR": _data[2][3],
+          "AR": _data[4][3]
+        },
+        "commercial_name": _data[2][4],
+        "address": {
+          "FR": _data[2][5],
+          "AR": _data[4][5]
+        }
+      };
+      return result;
+    }
   }
 };
--- tnrc.test.js
+++ tnrc.test.js
@@ -1,15 +1,15 @@
-const rc = require('./tnrc');
+const { tnrc } = require('./tnrc');
 
 it('Get company name by RC number.', async () => {
   window.jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
-  const d = await rc.getDataByRC('B11207432014');
-  expect(d['company_name']['FR']).toBe('STE EYE CATCHING DESIGN');
-  expect(d['legal_form']).toBe('SARL');
-  expect(d['reg_status']).toBe('Actif');
+  const data = await tnrc.getDataByRC('B11207432014');
+  expect(data['company_name']['FR']).toBe('STE EYE CATCHING DESIGN');
+  expect(data['legal_form']).toBe('SARL');
+  expect(data['reg_status']).toBe('Actif');
 });
 
 it('No results use case.', async () => {
   window.jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-  const d = await rc.getDataByRC('NONE');
-  expect(d).toBe(false);
+  const data = await tnrc.getDataByRC('NONE');
+  expect(data).toBe(false);
 });
