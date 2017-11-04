ca51a988fb2e4e75edbd79e3fcac3db639eb6533
tarekdj <tarekdj@gmail.com>
2017-11-04 14:13:14 +0100

    Add search by MF number.

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
   URL: 'http://www.registre-commerce.tn/search/RCCSearch.do?action=getPage&rg_type=PM&search_mode=NORMAL',
-  SEARCH_INPUT: 'input[name="searchRegistrePmRcc.numRegistre"]',
+  RC_SEARCH_INPUT: 'input[name="searchRegistrePmRcc.numRegistre"]',
+  MF_SEARCH_INPUT: 'input[name="searchRegistrePmRcc.registrePm.numPatente"]',  
   SEACH_RESULT_SELECTOR: '.search-container .send-btn-search > div.child-padding',
   VIEW_LINK: 'a[href="javascript:choixDocuments(0);"]',
   TBL_SELECTOR: '.tbl-measurement'
--- tnrc.test.js
+++ tnrc.test.js
@@ -8,8 +8,22 @@ it('Get company name by RC number.', async () => {
   expect(data['reg_status']).toBe('Actif');
 });
 
-it('No results use case.', async () => {
+it('No results found fo RC number.', async () => {
   window.jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-  const data = await tnrc.getDataByRC('NONE');
-  expect(data).toBe(false);
+  const no_rc = await tnrc.getDataByRC('NONE');
+  expect(no_rc).toBe(false);
+});
+
+it('Get company name by MF number.', async () => {
+  window.jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+  const data = await tnrc.getDataByMF('1371092R');
+  expect(data['company_name']['FR']).toBe('STE EYE CATCHING DESIGN');
+  expect(data['legal_form']).toBe('SARL');
+  expect(data['reg_status']).toBe('Actif');
+});
+
+it('No results found fo MF number.', async () => {
+  window.jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+  const no_mf = await tnrc.getDataByMF('NONE');
+  expect(no_mf).toBe(false);
 });
